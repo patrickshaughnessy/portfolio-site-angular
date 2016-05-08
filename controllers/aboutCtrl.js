2b4c2cae12b28ebf05cf1afbad95ad74c98e47ea
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-08 15:10:19 -0700

    messing with the theme

--- controllers/aboutCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular
-  .module('app')
-  .controller('aboutCtrl', function($scope, $timeout){
-
-  })
--- controllers/homeCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular
-  .module('app')
-  .controller('homeCtrl', function($rootScope, $scope, $timeout){
-  })
